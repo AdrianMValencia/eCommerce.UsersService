68e6e22ded9241a48327814b5d4cf6d5fdf8b12d
Adrian Mechato <cesar.mechatov@externos.bci.cl>
2025-12-20 12:45:26 -0500

    [UPDATE]

--- src/eCommerce.UsersService.Api/Contracts/Users/AuthenticationResponse.cs
+++ src/eCommerce.UsersService.Api/Contracts/Users/AuthenticationResponse.cs
@@ -8,3 +8,11 @@ public class AuthenticationResponse
     public string Email { get; set; } = null!;
     public string Token { get; set; } = null!;
 }
+
+public class UserResponse
+{
+    public Guid UserID { get; set; }
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string Email { get; set; } = null!;
+}
---
+++ src/eCommerce.UsersService.Api/Features/Users/GetUserById.cs
@@ -0,0 +1,86 @@
+﻿using Carter;
+using eCommerce.UsersService.Api.Abstractions.Messaging;
+using eCommerce.UsersService.Api.Contracts.Users;
+using eCommerce.UsersService.Api.Database;
+using eCommerce.UsersService.Api.Shared.Bases;
+using Mapster;
+using Microsoft.EntityFrameworkCore;
+
+namespace eCommerce.UsersService.Api.Features.Users;
+
+public class GetUserById
+{
+    #region Query
+    public sealed class Query : IQuery<UserResponse>
+    {
+        public Guid UserId { get; set; }
+    }
+    #endregion
+
+    #region Handler
+    internal sealed class Handler : IQueryHandler<Query, UserResponse>
+    {
+        private readonly ApplicationDbContext _context;
+
+        public Handler(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<BaseResponse<UserResponse>> Handle(Query query, CancellationToken cancellationToken)
+        {
+            var response = new BaseResponse<UserResponse>();
+
+            try
+            {
+                var user = await _context.Users.
+                    FirstOrDefaultAsync(u => u.UserID == query.UserId, cancellationToken);
+
+                if (user is null)
+                {
+                    response.IsSuccess = false;
+                    response.Message = "User not found.";
+                    return response;
+                }
+
+                var userResponse = user.Adapt<UserResponse>();
+
+                response.IsSuccess = true;
+                response.Data = userResponse;
+                response.Message = "User retrieved successfully.";
+            }
+            catch (Exception ex)
+            {
+                response.IsSuccess = false;
+                response.Message = $"An error occurred while retrieving the user. {ex.Message}";
+            }
+
+            return response;
+        }
+    }
+    #endregion
+
+    #region Endpoint
+    public class GetByIdUserEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("api/users/{userId}", async (
+                Guid userId,
+                IDispatcher dispatcher,
+                CancellationToken cancellationToken) =>
+            {
+                var query = new Query { UserId = userId };
+
+                var response = await dispatcher
+                    .Dispatch<Query, UserResponse>(query, cancellationToken);
+
+                if (!response.IsSuccess)
+                    return Results.NotFound(response);
+
+                return Results.Ok(response);
+            });
+        }
+    }
+    #endregion
+}
